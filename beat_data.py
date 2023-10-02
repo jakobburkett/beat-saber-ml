dc62c879fcf2909e1b7c559c767f8f39adc88059
Jakob Burkett <jb410817@ohio.edu>
2023-10-02 12:29:10 -0400

    added some more class stuff (unfinished)

--- beat_data.py
+++ beat_data.py
@@ -1,5 +1,19 @@
 # start for a class that has the info in the info.dat files (may change based on file version?)
 
+class Difficulty_Beatmap_Sets:
+    def __init__(self, difficulty, difficulty_rank, beatmap_filename)
+        # these are the things that make up one
+        # difficulty_rank = 1,
+        #         beatmap_filename = "EasyStandard.dat",
+        #         note_jump_movement_speed = 10,
+        #         note_jump_start_beat_offset = 1.0399999618530273,
+        #         editor_offset = 0,
+        #         editor_old_offset = 0,
+        #         warnings = ["EpilepsyWarning: Map contains flashing lights"],
+        #         information = [],
+        #         suggestions = [],
+        #         requirements = []
+
 class Info:
     def __init__(self, version = None, song_name = None, song_sub_name = None, 
                  song_author = None, level_author = None, bpm = None, song_file = None,
@@ -20,3 +34,66 @@ class Info:
         self.song_time_offset = 0 if song_time_offset == None else song_time_offset
         # self.customData:
         self.contributors = []
+        # probably able to do this with just a difficulty variable, all varables in the value of the dict is the same
+        # self.difficulty_beatmap_sets = {
+        #     "Easy": [
+        #         difficulty_rank = 1,
+        #         beatmap_filename = "EasyStandard.dat",
+        #         note_jump_movement_speed = 10,
+        #         note_jump_start_beat_offset = 1.0399999618530273,
+        #         editor_offset = 0,
+        #         editor_old_offset = 0,
+        #         warnings = ["EpilepsyWarning: Map contains flashing lights"],
+        #         information = [],
+        #         suggestions = [],
+        #         requirements = []
+        #     ],
+        #     "Normal": [
+        #         difficulty_rank = 3,
+        #         beatmap_filename = "NormalStandard.dat",
+        #         note_jump_movement_speed = 10,
+        #         note_jump_start_beat_offset = 0.9900000095367432,
+        #         editor_offset = 0,
+        #         editor_old_offset = 0,
+        #         warnings = ["EpilepsyWarning: Map contains flashing lights"],
+        #         information = [],
+        #         suggestions = [],
+        #         requirements = []
+        #     ],
+        #     "Hard": [
+        #         difficulty_rank = 5,
+        #         beatmap_filename = "HardStandard.dat",
+        #         note_jump_movement_speed = 13,
+        #         note_jump_start_beat_offset = 0.2590000033378601,
+        #         editor_offset = 0,
+        #         editor_old_offset = 0,
+        #         warnings = ["EpilepsyWarning: Map contains flashing lights"],
+        #         information = [],
+        #         suggestions = [],
+        #         requirements = []
+        #     ],
+        #     "Expert": [
+        #         difficulty_rank = 7,
+        #         beatmap_filename = "ExpertStandard.dat",
+        #         note_jump_movement_speed = 13,
+        #         note_jump_start_beat_offset = -0.20000000298023224,
+        #         editor_offset = 0,
+        #         editor_old_offset = 0,
+        #         warnings = ["EpilepsyWarning: Map contains flashing lights"],
+        #         information = [],
+        #         suggestions = [],
+        #         requirements = []
+        #     ],
+        #     "ExpertPlus": [
+        #         difficulty_rank = 9,
+        #         beatmap_filename = "ExpertPlusStandard.dat",
+        #         note_jump_movement_speed = 19,
+        #         note_jump_start_beat_offset = -0.5,
+        #         editor_offset = 0,
+        #         editor_old_offset = 0,
+        #         warnings = ["EpilepsyWarning: Map contains flashing lights"],
+        #         information = [],
+        #         suggestions = [],
+        #         requirements = []
+        #     ]
+        # }
